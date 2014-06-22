b044c7d98434a78bb04a9f9d5e1ecd843e12824e
Huang, Xin <xinhuang@thoughtworks.com>
2014-06-22 20:36:46 +0800

    code clean

--- web-api-test/HelloWorldControllerTest.cs
+++ web-api-test/HelloWorldControllerTest.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using NUnit.Framework;
 
 namespace web_api_test
